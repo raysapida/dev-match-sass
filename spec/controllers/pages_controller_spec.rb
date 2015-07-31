37d9d27c441d4f1101dce777bc3a424ce9d65e65
Raymond Sapida <ray.sapida@gmail.com>
2015-07-31 15:20:57 -0700

    Add about page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,15 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-  render_views
+  context 'GET home' do
+    render_views
 
-  it 'gives a success response when going to home' do
-    get :home
-    expect(response.status).to eq(200)
+    it 'gives a success response when going to home' do
+      get :home
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders home template' do
+      get :home
+      expect(response.body).to match(/Home/im)
+    end
   end
 
-  it 'renders home template' do
-    get :home
-    expect(response.body).to match(/Home/im)
+  context 'GET about' do
+    render_views
+
+    it 'gives a success response when going to home' do
+      get :about
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders home template' do
+      get :about
+      expect(response.body).to match(/About/im)
+    end
   end
 end
