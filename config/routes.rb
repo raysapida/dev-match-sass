e29a61bb129f9ec5a3e9286633d02dcfa5a53ef6
Raymond Sapida <ray.sapida@gmail.com>
2015-07-31 19:06:08 -0700

    Add profile nested routes and profiles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'pages#home'
+  resources :users do
+    resource :profile
+  end
   resources :contacts, only: [:new, :create]
   get '/about' => 'pages#about'
 end
