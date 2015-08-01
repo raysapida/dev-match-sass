d09a7cd056bd849e06d9183dd973c60e0b1371cf
Raymond Sapida <ray.sapida@gmail.com>
2015-07-31 18:55:10 -0700

    Add stripe customer token to users and update devise controller to permit that field

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'pages#home'
   resources :contacts, only: [:new, :create]
   get '/about' => 'pages#about'
