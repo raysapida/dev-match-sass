a628806d85fc05afa2d996b1ad9494c16dc833d2
Raymond Sapida <ray.sapida@gmail.com>
2015-08-15 18:20:12 -0700

    Add paperclip matchers to test attachment content type for the avtar field

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
   it { should belong_to :user }
+  it { should validate_attachment_content_type(:avatar).
+       allowing('image/png', 'image/gif', 'image/jpeg', 'image/bmp').
+       rejecting('text/plain', 'text/xml') }
 end
